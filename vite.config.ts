08ebbd392915416eb4236250221f19a599c58d40
Abir-Bekakria <abirbekakria5@gmail.com>
2026-04-28 17:08:44 +0200

    initial commith

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: "piece",
+  base: "/piece/"
 });
